43b74d92aad9e1c74c6d422a20a05d1240a1f809
AlexKon060698 <aleksander.kononenko2016@yandex.ru>
2024-08-18 16:22:46 +0300

    динамическое чтение папки

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
-const convertToPng = require('./pdftopng');
-const path = require('path');
+const fs = require('fs');
+const { pdfToPng } = require('pdf-to-png-converter');
 
-convertToPng(path.join(__dirname, './pdf_files/АТТЕСТАТ ФАРМЛНИК.pdf'), 2);
+const readDir = (path) => {
+  return fs.readdirSync(__dirname + path, {});
+};
+
+const makePath = (path, i) => {
+  path.replace(__dirname, '').replace('\\dir', '').replace('.pdf', '') + `-${i}.png`
+}
+
+const convertPdfToImg = async (buffer, pages) => {
+  let pagesArr = Array(pages).fill().map((e, i) => i + 1);
+
+  const pngPage = await pdfToPng(buffer, {
+    disableFontFace: true,
+    useSystemFonts: true,
+    pagesToProcess: pagesArr,
+    viewportScale: 2.0
+  });
+
+  return pngPage;
+};
+
+const convertToPng = async (pathToFile, pages) => {
+  if (fs.existsSync(pathToFile)) {
+    const pdf = fs.readFileSync(pathToFile);
+    const pngs = await convertPdfToImg(pdf, pages);
+    pngs.forEach((png, i) => {
+      fs.writeFileSync(__dirname + `\\png_files` + makePath(pathToFile, i), png.content);
+    });
+  }
+};
+
+// TODO...
+const files = readDir('/dir');
+
+files.forEach(async (file) => {
+  await convertToPng(__dirname + '\\dir\\' + file, 100);
+});
